25cf815939b2e48089456a96874672f71159eaee
Colin Jaffe <balloonasaurus@gmail.com>
2021-05-11 17:44:56 -0400

    Adds comment on unnecessary state.

--- src/components/CalcResults.js
+++ src/components/CalcResults.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 export default class CalcResults extends Component {
+    // There's no need for this component to have state... it's being passed the result as a prop!
+    // You can skip putting it in state and formatting that state. You're doing a great job by
+    // simply formatting the prop using `.toLocaleString`.
     constructor() {
         super() 
 
